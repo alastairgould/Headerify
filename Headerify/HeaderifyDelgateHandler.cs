7e74c9ac73406149973779a3deeded5f81733298
Alastair Gould <alastairgould@gmail.com>
2019-05-14 23:16:34 +0100

    add tests

--- Headerify/HeaderifyDelgateHandler.cs
+++ Headerify/HeaderifyDelgateHandler.cs
@@ -9,15 +9,17 @@ namespace Headerify
     {
         private readonly HeaderifyOptions _headerifyOptions;
 
-        public HeaderifyDelgateHandler(HeaderifyOptions generateToken)
+        public HeaderifyDelgateHandler(HeaderifyOptions headerifyOptions)
         {
+            _headerifyOptions = headerifyOptions;
         }
 
         protected override async Task<HttpResponseMessage> SendAsync(
             HttpRequestMessage request,
             CancellationToken cancellationToken)
         {
-            return null;
+            request.Headers.Add(_headerifyOptions.HeaderName, _headerifyOptions.HeaderValue);
+            return await base.SendAsync(request, cancellationToken);
         }
     }
 }
